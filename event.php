02e17ff4ef08588f957162375393524827aea45b
kishan vasani <70906939+mrvasani48@users.noreply.github.com>
2021-06-11 01:46:35 +0530

    Add files via upload

---
+++ event.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="event.css">
+</head>
+<body>
+<?php require_once 'header.php'; ?>
+    <div class="newslattwemain">
+        <div class="yearnews">
+
+            <table style="border-collapse: collapse; width:80%;" border="1">
+                <tr class="yearhover">
+                    <th style=" color: white; padding: 15px; font-size: 25px;   background-color: #003975; ">
+                        Events @ GECR
+                    </th>
+                </tr>
+                <tr class="yearhover yearn active">
+                    <td style=" padding:10px; ">2021</td>
+                </tr>
+                <tr class="yearhover yearn">
+                    <td style=" padding:10px; ">2020</td>
+                </tr>
+                <tr class="yearhover yearn">
+                    <td style=" padding:10px; ">2019</td>
+                </tr>
+                <tr class="yearhover yearn">
+                    <td style=" padding:10px; ">2018</td>
+                </tr>
+                <tr class="yearhover yearn">
+                    <td style=" padding:10px; ">2017</td>
+                </tr>
+                <tr class="yearhover yearn">
+                    <td style=" padding:10px; ">2016</td>
+                </tr>
+            </table>
+
+        </div >
+        <div class="event2main">        
+              <h1 class="decnewdiv1"> Events @ GECR</h1>
+
+              <div class="even">
+                <div class="event1ma"> 
+                     <img class="imgeven1" height="300px" width="300px" src="http://alumnigecr.com/images/event_list/event_image/event_list_img_9d0cf0.jpg" alt="" srcset="">
+                     <p  class="txteven1"><a  style="text-decoration: none; color: #003975;" href="http://alumnigecr.com/event-detail/10/"> Alumni Associatio Group (AAG) of GEC -Rajkot had launched the Alumni Android App.</a></p> 
+                </div> 
+                <div class="event2ma"> 
+                    <img class="imgeven1" height="300px" width="300px" src="http://alumnigecr.com/images/event_list/event_image/event_list_img_a4c35e.jpg" alt="" srcset="">
+                    <p  class="txteven1"><a style="text-decoration: none; color: #003975;" href="http://alumnigecr.com/event-detail/14/" class="txteven1"> ideation 2021</a></p> 
+               </div>      
+              </div>
+
+                <div class="even">
+                <div class="event1ma"> 
+                     <img class="imgeven1" height="300px" width="300px" src="http://alumnigecr.com/images/event_list/event_image/event_list_img_d41184.jpg" alt="" srcset="">
+                     <p  class="txteven1"> <a style="text-decoration: none; color: #003975;" href="http://alumnigecr.com/event-detail/16/" class="txteven1"> Webinar on Chase your dream of Master Abroad</a></p> 
+                </div> 
+                <div class="event2ma"> 
+                    <img class="imgeven1" height="300px" width="300px" src="http://alumnigecr.com/images/event_list/event_image/event_list_img_234523.jpg" alt="" srcset="">
+                    <p  class="txteven1"> <a style="text-decoration: none; color: #003975;" href="http://alumnigecr.com/event-detail/19/" class="txteven1"> Listen the success story from GEC ALUMNI -SBI Recruitment 2021</a></p> 
+               </div>      
+              </div>
+        </div>
+    </div> 
+    <?php require_once 'footer.php'; ?>
+</body>
+</html>
